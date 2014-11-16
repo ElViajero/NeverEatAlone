3d08a97b7e59490ab33476fb363d9fbcf4baacf3
Hai Tang <htang16@jhu.edu>
2014-11-16 00:31:45 -0500

    Revised the listview in invites page to condensed listview

--- NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/gui/activities/InvitesActivity.java
+++ NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/gui/activities/InvitesActivity.java
@@ -1,60 +1,66 @@
 package edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.R;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.helpers.MealNotificationAdapter;
+
 /**
  * 
  * @author Hai Tang
  *
  */
 public class InvitesActivity extends Activity {
+	private ListView MealNotificationListView;
+	private ArrayAdapter MealNotificationArrayAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		initView(savedInstanceState);
-		
+
 	}
 
 	private void initView(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_invites);
-		
-		final ListView listview = (ListView) findViewById(R.id.listView_invitations);
-	    String[] values = new String[] { "Invitation 1", "Invitation2", "Invitation3" };
 
-	    final ArrayList<String> list = new ArrayList<String>();
-	    for (int i = 0; i < values.length; ++i) {
-	      list.add(values[i]);
-	    }
-	    final StableArrayAdapter adapter = new StableArrayAdapter(this,
-	        android.R.layout.simple_list_item_1, list);
-	    listview.setAdapter(adapter);
+		MealNotificationArrayAdapter = new MealNotificationAdapter(this,
+				new String[10]);
+		MealNotificationListView = (ListView) findViewById(R.id.listView_mealnotification);
+		MealNotificationListView.setAdapter(MealNotificationArrayAdapter);
 
-	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		/*
+		 * The following code use StableArrayAdapter to show a list of view, disappears after clicking
+		 * 
+		 * final ListView listview = (ListView)
+		 * findViewById(R.id.listView_invitations); String[] values = new
+		 * String[] { "Invitation 1", "Invitation2", "Invitation3" };
+		 * 
+		 * final ArrayList<String> list = new ArrayList<String>(); for (int i =
+		 * 0; i < values.length; ++i) { list.add(values[i]); } final
+		 * StableArrayAdapter adapter = new StableArrayAdapter(this,
+		 * android.R.layout.simple_list_item_1, list);
+		 * listview.setAdapter(adapter);
+		 * 
+		 * listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		 * {
+		 * 
+		 * @Override public void onItemClick(AdapterView<?> parent, final View
+		 * view, int position, long id) { final String item = (String)
+		 * parent.getItemAtPosition(position);
+		 * view.animate().setDuration(2000).alpha(0) .withEndAction(new
+		 * Runnable() {
+		 * 
+		 * @Override public void run() { list.remove(item);
+		 * adapter.notifyDataSetChanged(); view.setAlpha(1); } }); }
+		 * 
+		 * });
+		 */
 
-	      @Override
-	      public void onItemClick(AdapterView<?> parent, final View view,
-	          int position, long id) {
-	        final String item = (String) parent.getItemAtPosition(position);
-	        view.animate().setDuration(2000).alpha(0)
-	            .withEndAction(new Runnable() {
-	              @Override
-	              public void run() {
-	                list.remove(item);
-	                adapter.notifyDataSetChanged();
-	                view.setAlpha(1);
-	              }
-	            });
-	      }
-
-	    });
 	}
 
 	public void OnCreateButtonClick(View view) {
