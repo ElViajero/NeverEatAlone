b72c87da1af310583161af8f5f981d44052b2e2f
yueling <yueling.loh@gmail.com>
2014-12-05 09:10:40 -0500

    Restored RegisterActivity file (that was deleted in last commit)

---
+++ NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/gui/activities/RegisterActivity.java
@@ -0,0 +1,150 @@
+package edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.impl.execchain.RequestAbortedException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.R;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.activityProperties.services.AccountProperties;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.helpers.MessageToasterHelper;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.requestHandler.services.RequestHandlerHelper;
+
+/**
+ * This class handles controller logic for the registration Activity.
+ * 
+ * @author tejasvamsingh,
+ * @author Yueling Loh
+ *
+ */
+public class RegisterActivity extends Activity {
+
+	// fields used by the register activity.
+	private EditText usernameET;
+	private EditText emailET;
+	private EditText passwordET;
+	private EditText confirmPasswordET;
+	private String requestType;
+	private String requestID;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+
+		// binding the fields to class variables.
+		usernameET = (EditText) findViewById(R.id.edit_username);
+		passwordET = (EditText) findViewById(R.id.edit_password);
+		confirmPasswordET = (EditText) findViewById(R.id.edit_confirm_password);
+		emailET = (EditText) findViewById(R.id.edit_email);
+
+		// set the RequestType and RequestID fields.
+		requestID = "Account";
+		requestType = "Create";
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.register, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Event Handler method for the register button
+	 * 
+	 * @author tejasvamsingh
+	 * @author Yueling Loh
+	 * @param view
+	 */
+	public void OnRegisterButtonClick(View view) {
+
+		// Fetch the fields from the GUI.
+		String username = usernameET.getText().toString();
+		String password = passwordET.getText().toString();
+		String email = emailET.getText().toString();
+		String confirmPassword = confirmPasswordET.getText().toString();
+
+		System.out.println(password);
+		System.out.println(confirmPassword);
+
+		// Check if entries are valid
+		boolean validRegistration = checkValidRegistration(username, password,
+				email, confirmPassword);
+		if (!validRegistration)
+			return;
+
+		// Create a properties object.
+		AccountProperties registerProperties = new AccountProperties(username,
+				password, email);
+
+		// Get the request Map
+		Map<String, Object> requestMap = registerProperties.toMap();
+
+		try {
+			// Initiate the request.
+			List<Map<String, String>> resultMapList = RequestHandlerHelper
+					.getRequestHandlerInstance().handleRequest(this,
+							requestMap, requestID, requestType);
+			// Handle the result.
+			MessageToasterHelper.toastMessage(this, "Registration Successful");
+			Intent intent = new Intent(RegisterActivity.this,
+					MainActivity.class);
+			RegisterActivity.this.startActivity(intent);
+		} catch (RequestAbortedException e) {
+			return;
+		}
+
+	}
+
+	private boolean checkValidRegistration(String username, String password,
+			String email, String confirmPassword) {
+
+		if (username.equals("")) {
+			MessageToasterHelper.toastMessage(this, R.string.empty_username);
+			return false;
+		}
+
+		if (email.equals("")) {
+			MessageToasterHelper.toastMessage(this, R.string.empty_email);
+			return false;
+		}
+
+		if (password.equals("")) {
+			MessageToasterHelper.toastMessage(this, R.string.empty_password);
+			return false;
+		}
+
+		if (!password.equals(confirmPassword)) {
+			MessageToasterHelper.toastMessage(this,
+					R.string.different_passwords);
+			return false;
+		}
+		return true;
+	}
+
+	public void OnCancelButtonClick(View view) {
+		Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+		RegisterActivity.this.startActivity(intent);
+	}
+}
