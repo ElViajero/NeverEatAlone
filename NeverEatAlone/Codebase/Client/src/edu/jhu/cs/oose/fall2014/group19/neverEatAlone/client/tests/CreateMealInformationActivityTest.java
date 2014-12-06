e79ee203950ae85de88e2d97117418abd215cd78
yueling <yueling.loh@gmail.com>
2014-12-05 20:15:31 -0500

    CreateMealInfoActivity - Partial

--- NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/CreateMealInformationActivityTest.java
+++ NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/CreateMealInformationActivityTest.java
@@ -2,9 +2,11 @@ package edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.tests;
 
 import com.robotium.solo.Solo;
 
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.R;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.CreateMealInformationActivity;
 import android.test.ActivityInstrumentationTestCase2;
-
+import android.view.View;
+import android.widget.DatePicker;
 
 /**
  * This class tests the activities on the CreateMealInformationActivitiy
@@ -14,31 +16,90 @@ import android.test.ActivityInstrumentationTestCase2;
  */
 public class CreateMealInformationActivityTest extends
 		ActivityInstrumentationTestCase2<CreateMealInformationActivity> {
-	
-	private Solo solo; 
-	
+
+	private Solo solo;
+
 	public CreateMealInformationActivityTest() {
 		super(CreateMealInformationActivity.class);
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		solo = new Solo(getInstrumentation(),getActivity());
+		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
 	protected void tearDown() throws Exception {
 		solo.finishOpenedActivities();
 		super.tearDown();
 	}
+
+	// Test if start date is shown correctly
+	public void testStartDate() throws Exception {
+		// Check we have the right activity
+		solo.assertCurrentActivity("Wrong Activity",
+				CreateMealInformationActivity.class);
+
+		// Check if button displays correctly
+		View button = solo.getView(R.id.CreateMealInformation_button_startDate);
+		solo.clickOnView(button);
+		solo.setDatePicker(0, 2014, 12, 17);
+		solo.clickOnText("Done");
+		assertTrue(solo.searchText("17-12-2014"));
+
+	}
 	
-	// Test if start date is picked right
-	// Test if start time is picked right
-	// Test if end date is picked right
-	// Test if end time is correct
+	// Test if end date is shown correctly
+	public void testEndDate() throws Exception {
+		// Check we have the right activity
+		solo.assertCurrentActivity("Wrong Activity",
+				CreateMealInformationActivity.class);
+
+		// Check if button displays correctly
+		View button = solo.getView(R.id.CreateMealInformation_button_endDate);
+		solo.clickOnView(button);
+		solo.setDatePicker(0, 2014, 12, 18);
+		solo.clickOnText("Done");
+		assertTrue(solo.searchText("18-12-2014"));
+
+	}
+
+	// Test if start time is shown correctly
+	public void testStartTime() throws Exception {
+		// Check we have the right activity
+		solo.assertCurrentActivity("Wrong Activity",
+				CreateMealInformationActivity.class);
+
+		// Check if button displays correctly
+		View button = solo.getView(R.id.CreateMealInformation_button_startTime);
+		solo.clickOnView(button);
+		solo.setTimePicker(0, 5, 31);
+		solo.clickOnText("Done");
+		assertTrue(solo.searchText("5:31"));
+
+	}
+
+	// Test if end time is shown correctly
+	public void testEndTime() throws Exception {
+		// Check we have the right activity
+		solo.assertCurrentActivity("Wrong Activity",
+				CreateMealInformationActivity.class);
+
+		// Check if button displays correctly
+		View button = solo.getView(R.id.CreateMealInformation_button_endTime);
+		solo.clickOnView(button);
+		solo.setTimePicker(0, 17, 31);
+		solo.clickOnText("Done");
+		assertTrue(solo.searchText("17:31"));
+
+	}
+	
 	// Test if end date is bigger or equal to start date
 	// Test if end time is bigger or equal to start time
 	// Test if fails if Restaurant is empty
 	// Test if Maximum Number Attendance is int > 0
-
+	
+	// Test if allow friends slider is working
+	// Test if Back button is working
+	// Test if Next button is working
 	
 }
