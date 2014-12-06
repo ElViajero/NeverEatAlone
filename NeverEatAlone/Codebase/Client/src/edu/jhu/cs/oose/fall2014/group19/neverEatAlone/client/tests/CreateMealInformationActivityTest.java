fbc81d756d95c6eca530b34f01d71ebc062da431
yueling <yueling.loh@gmail.com>
2014-12-05 21:41:11 -0500

    More test updates

--- NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/CreateMealInformationActivityTest.java
+++ NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/CreateMealInformationActivityTest.java
@@ -4,9 +4,10 @@ import com.robotium.solo.Solo;
 
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.R;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.CreateMealInformationActivity;
+import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.TabHostActivity;
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
-import android.widget.DatePicker;
 
 /**
  * This class tests the activities on the CreateMealInformationActivitiy
@@ -33,7 +34,12 @@ public class CreateMealInformationActivityTest extends
 		super.tearDown();
 	}
 
-	// Test if start date is shown correctly
+	/**
+	 * Test if start date is shown correctly
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testStartDate() throws Exception {
 		// Check we have the right activity
 		solo.assertCurrentActivity("Wrong Activity",
@@ -47,8 +53,13 @@ public class CreateMealInformationActivityTest extends
 		assertTrue(solo.searchText("17-12-2014"));
 
 	}
-	
-	// Test if end date is shown correctly
+
+	/**
+	 * Test if end date is shown correctly
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testEndDate() throws Exception {
 		// Check we have the right activity
 		solo.assertCurrentActivity("Wrong Activity",
@@ -63,7 +74,12 @@ public class CreateMealInformationActivityTest extends
 
 	}
 
-	// Test if start time is shown correctly
+	/**
+	 * Tests if start time is shown correctly
+	 * 
+	 * @author Yueling Loh
+	 * 
+	 * */
 	public void testStartTime() throws Exception {
 		// Check we have the right activity
 		solo.assertCurrentActivity("Wrong Activity",
@@ -78,7 +94,12 @@ public class CreateMealInformationActivityTest extends
 
 	}
 
-	// Test if end time is shown correctly
+	/**
+	 * Test if end time is shown correctly
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testEndTime() throws Exception {
 		// Check we have the right activity
 		solo.assertCurrentActivity("Wrong Activity",
@@ -92,14 +113,102 @@ public class CreateMealInformationActivityTest extends
 		assertTrue(solo.searchText("17:31"));
 
 	}
-	
+
 	// Test if end date is bigger or equal to start date
 	// Test if end time is bigger or equal to start time
-	// Test if fails if Restaurant is empty
+
+	// /**
+	// * Test if fails when Restaurant field is empty
+	// *
+	// * @author Yueling Loh
+	// *
+	// */
+	// public void testEmptyRestaurant() {
+	// EditText vRestaurantName = (EditText) solo
+	// .getView(R.id.edit_restaurant);
+	//
+	// setStartEndDatesAndTimes();
+	// solo.enterText(vRestaurantName, "");
+	//
+	// solo.clickOnButton("Next");
+	// solo.assertCurrentActivity(
+	// "Should remain on page, since no Restaurant inputed.",
+	// CreateMealInformationActivity.class);
+	//
+	// }
+
 	// Test if Maximum Number Attendance is int > 0
-	
-	// Test if allow friends slider is working
-	// Test if Back button is working
-	// Test if Next button is working
-	
+
+	/**
+	 * Test if allow friends slider is working
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
+	public void testAllowFriendInvite() {
+		Activity a = getActivity();
+		String switchName = a.getString(R.string.capital_no);
+		solo.clickOnToggleButton(switchName);
+		switchName = a.getString(R.string.capital_yes);
+		solo.clickOnToggleButton(switchName);
+	}
+
+	/**
+	 * Test if back button is working
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
+	public void testOnBackButtonClick() {
+		solo.clickOnButton("Back");
+		solo.assertCurrentActivity(
+				"Should go back to main tabs page when canceled",
+				TabHostActivity.class);
+	}
+
+	// /**
+	// * Test if Next button is working, assuming that fields have been properly
+	// * filled
+	// *
+	// * @author Yueling Loh
+	// *
+	// */
+	// public void testOnNextButtonClick() {
+	// // Check we have the right activity
+	// solo.assertCurrentActivity("Wrong Activity",
+	// CreateMealInformationActivity.class);
+	//
+	// setStartEndDatesAndTimes();
+	//
+	// solo.clickOnButton("Next");
+	// solo.assertCurrentActivity(
+	// "Should go back to Select Friends page when finished",
+	// SelectFriendsActivity.class);
+	// }
+
+	// private void setStartEndDatesAndTimes() {
+	// // Select start date
+	// View button = solo.getView(R.id.CreateMealInformation_button_startDate);
+	// solo.clickOnView(button);
+	// solo.setDatePicker(0, 2014, 12, 17);
+	// solo.clickOnText("Done");
+	//
+	// // Select start time
+	// button = solo.getView(R.id.CreateMealInformation_button_startTime);
+	// solo.clickOnView(button);
+	// solo.setTimePicker(0, 17, 30);
+	// solo.clickOnText("Done");
+	//
+	// // Select end date
+	// button = solo.getView(R.id.CreateMealInformation_button_endDate);
+	// solo.clickOnView(button);
+	// solo.setDatePicker(0, 2014, 12, 17);
+	// solo.clickOnText("Done");
+	//
+	// // Select end time
+	// button = solo.getView(R.id.CreateMealInformation_button_endTime);
+	// solo.clickOnView(button);
+	// solo.setTimePicker(0, 18, 30);
+	// solo.clickOnText("Done");
+	// }
 }
--- NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/RegisterActivityTest.java
+++ NeverEatAlone/Codebase/Client/src/edu/jhu/cs/oose/fall2014/group19/neverEatAlone/client/tests/RegisterActivityTest.java
@@ -5,6 +5,7 @@ import com.robotium.solo.Solo;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.R;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.MainActivity;
 import edu.jhu.cs.oose.fall2014.group19.neverEatAlone.client.gui.activities.RegisterActivity;
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
 
@@ -35,8 +36,40 @@ public class RegisterActivityTest extends
 	}
 
 	/**
-	 * Tests registration with an existing account (duplicate username)
-	 * (registration should fail)
+	 * Tests if the register button works, assuming fields were correctly
+	 * inputed.
+	 * 
+	 * @author Xiaozhou Zhou
+	 * @author Yueling Loh
+	 *
+	 */
+	public void testOnRegisterButtonClick() throws Exception {
+		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
+		EditText vEditPassword = (EditText) solo.getView(R.id.edit_password);
+		EditText vConfirmPassword = (EditText) solo
+				.getView(R.id.edit_confirm_password);
+		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
+
+		// create a test account if it does not already exist
+		solo.enterText(vUserName, "usr_test_ok");
+		solo.enterText(vEmail, "email_ok@test.com");
+		solo.enterText(vEditPassword, "pw_test");
+		solo.enterText(vConfirmPassword, "pw_test");
+		solo.clickOnButton("Register");
+
+		solo.assertCurrentActivity(
+				"Registration passes; Should return to Main Page.",
+				MainActivity.class);
+
+	}
+
+	/**
+	 * Tests if registration fails, given that the username has already been
+	 * used.
+	 * 
+	 * @author Xiaozhou Zhou
+	 * @author Yueling Loh
+	 *
 	 */
 	public void testDuplicateUser() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
@@ -71,8 +104,11 @@ public class RegisterActivityTest extends
 	}
 
 	/**
-	 * Tests registration with an existing account (duplicate email)
-	 * (registration should fail)
+	 * Tests if registration fails, given that the email has already been used.
+	 * 
+	 * @author Xiaozhou Zhou
+	 * @author Yueling Loh
+	 *
 	 */
 	public void testDuplicateEmail() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
@@ -106,8 +142,10 @@ public class RegisterActivityTest extends
 	}
 
 	/**
-	 * Tests registration with incorrectly filled registration fields
-	 * (registration should fail)
+	 * Tests if registration fails, given that the passwords do not match.
+	 * 
+	 * @author Yueling Loh
+	 *
 	 */
 	public void testPasswordsDifferent() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
@@ -115,6 +153,9 @@ public class RegisterActivityTest extends
 		EditText vConfirmPassword = (EditText) solo
 				.getView(R.id.edit_confirm_password);
 		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
+		
+		Activity a = getActivity();
+		String stringResponse = a.getString(R.string.different_passwords);
 
 		// Registration should fail when passwords do not match
 		clearAllFields(vUserName, vEditPassword, vConfirmPassword, vEmail);
@@ -126,18 +167,26 @@ public class RegisterActivityTest extends
 		solo.assertCurrentActivity(
 				"Registration fails; Should stay on Register page.",
 				RegisterActivity.class);
-		boolean actual = solo.searchText("Passwords do not match.");
+		boolean actual = solo.searchText(stringResponse);
 		assertEquals("Passwords do not match -- not found", true, actual);
 
-
 	}
-	
+
+	/**
+	 * Tests if registration fails, given that no user name is provided.
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testNoUsername() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
 		EditText vEditPassword = (EditText) solo.getView(R.id.edit_password);
 		EditText vConfirmPassword = (EditText) solo
 				.getView(R.id.edit_confirm_password);
 		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
+		
+		Activity a = getActivity();
+		String stringResponse = a.getString(R.string.empty_username);
 
 		// Registration should fail when no user name is provided
 		clearAllFields(vUserName, vEditPassword, vConfirmPassword, vEmail);
@@ -148,17 +197,26 @@ public class RegisterActivityTest extends
 		solo.assertCurrentActivity(
 				"Registration fails; Should stay on Register page.",
 				RegisterActivity.class);
-		boolean actual = solo.searchText("No username provided.");
+		boolean actual = solo.searchText(stringResponse);
 		assertEquals("No username provided -- not found", true, actual);
 
 	}
-	
+
+	/**
+	 * Tests if registration fails, given that no email is provided.
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testNoEmail() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
 		EditText vEditPassword = (EditText) solo.getView(R.id.edit_password);
 		EditText vConfirmPassword = (EditText) solo
 				.getView(R.id.edit_confirm_password);
 		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
+		
+		Activity a = getActivity();
+		String stringResponse = a.getString(R.string.empty_email);
 
 		// Registration should fail when no email is provided
 		clearAllFields(vUserName, vEditPassword, vConfirmPassword, vEmail);
@@ -169,16 +227,25 @@ public class RegisterActivityTest extends
 		solo.assertCurrentActivity(
 				"Registration fails; Should stay on Register page.",
 				RegisterActivity.class);
-		boolean actual = solo.searchText("No email provided.");
+		boolean actual = solo.searchText(stringResponse);
 		assertEquals("No email provided -- not found", true, actual);
 	}
-	
+
+	/**
+	 * Tests if registration fails, given that no password is provided.
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testNoPassword() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
 		EditText vEditPassword = (EditText) solo.getView(R.id.edit_password);
 		EditText vConfirmPassword = (EditText) solo
 				.getView(R.id.edit_confirm_password);
 		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
+		
+		Activity a = getActivity();
+		String stringResponse = a.getString(R.string.empty_password);
 
 		// Registration should fail when no password is provided
 		clearAllFields(vUserName, vEditPassword, vConfirmPassword, vEmail);
@@ -189,10 +256,17 @@ public class RegisterActivityTest extends
 		solo.assertCurrentActivity(
 				"Registration fails; Should stay on Register page.",
 				RegisterActivity.class);
-		boolean actual = solo.searchText("No password provided.");
+		boolean actual = solo.searchText(stringResponse);
 		assertEquals("No password provided -- not found", true, actual);
 	}
 
+	/**
+	 * Tests if registration fails, given that both password fields are left
+	 * blank.
+	 * 
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testNoPasswordBoth() throws Exception {
 		EditText vUserName = (EditText) solo.getView(R.id.edit_username);
 		EditText vEditPassword = (EditText) solo.getView(R.id.edit_password);
@@ -200,6 +274,9 @@ public class RegisterActivityTest extends
 				.getView(R.id.edit_confirm_password);
 		EditText vEmail = (EditText) solo.getView(R.id.edit_email);
 
+		Activity a = getActivity();
+		String stringResponse = a.getString(R.string.empty_password);
+		
 		// Registration should fail when no password is provided (both fields)
 		clearAllFields(vUserName, vEditPassword, vConfirmPassword, vEmail);
 		solo.enterText(vUserName, "usr9_test");
@@ -208,11 +285,11 @@ public class RegisterActivityTest extends
 		solo.assertCurrentActivity(
 				"Registration fails; Should stay on Register page.",
 				RegisterActivity.class);
-		boolean actual = solo.searchText("No password provided.");
+		boolean actual = solo.searchText(stringResponse);
 		assertEquals("No password provided -- not found", true, actual);
 
 	}
-	
+
 	private void clearAllFields(EditText vUserName, EditText vEditPassword,
 			EditText vConfirmPassword, EditText vEmail) {
 		solo.clearEditText(vUserName);
@@ -221,6 +298,15 @@ public class RegisterActivityTest extends
 		solo.clearEditText(vEmail);
 	}
 
+	
+	/**
+	 * Tests if cancel button works
+	 * blank.
+	 * 
+	 * @author Xiaozhou Zhou
+	 * @author Yueling Loh
+	 *
+	 */
 	public void testOnCancelButtonClick() {
 		solo.clickOnButton("Cancel");
 		solo.assertCurrentActivity("Should go back to main page when canceled",
@@ -229,4 +315,3 @@ public class RegisterActivityTest extends
 	}
 
 }
-
